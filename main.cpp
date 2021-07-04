2e750b3721930f52c715e3dd81ddfb2ccec02ea9
Jaqueline Pereira <82723197+jaquelinepsilva@users.noreply.github.com>
2021-07-04 19:26:07 +0000

    criando a funçao que ira receber os dados

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
+using namespace std;
 
+struct tInvestimento{
+  float capital;
+  float taxaAnual;
+  int tempo;
+//receber dados
+};
+tInvestimento criaInvestimento(float capital,float taxaAnual,int tempo){
+
+tInvestimento novoInvestimento;
+
+novoInvestimento.capital=capital;
+novoInvestimento.taxaAnual=taxaAnual;
+novoInvestimento.tempo=tempo;
+
+return novoInvestimento;
+}
 int main() {
    
 }
